a8db129675bdf3d269d215cb4f5afdd8c588bf86
Ricardo Brazão <ricardo.p.brazao@gmail.com>
2017-02-07 19:20:25 +0000

    Add both migrations to create and drop the database
    Add both migrations to create and drop the database. Also the specs were not running locally due to the database not being setup properly.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,24 @@
 require 'rspec'
 require 'rails'
+require 'active_record'
 require 'systematize'
 require 'pry'
+
+DATABASE_SPEC_FILE = 'spec/support/db/test.sqlite3'
 
 RSpec.configure do |config|
   config.color = true
   config.tty = true
+
+  config.before(:suite) {
+    ActiveRecord::Base.establish_connection(
+      adapter: 'sqlite3',
+      database: DATABASE_SPEC_FILE,
+    )
+  }
+
+  config.after(:suite) {
+    FileUtils.rm(DATABASE_SPEC_FILE)
+    ActiveRecord::Base.connection.close
+  }
 end
